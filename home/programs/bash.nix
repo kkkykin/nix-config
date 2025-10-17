6d7cfcc3e44bbf13d9df631cb43502c2b6cee727
kkky <kkkykin@foxmail.com>
2025-10-17 21:20:52 +0800

    fix(bash): rc

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -6,7 +6,7 @@
     enable = true;
     enableCompletion = true;
     bashrcExtra = ''
-dot_rc='~/dotfiles/bash/_tangle/rc/general'
+dot_rc="$(realpath ~/dotfiles/bash/_tangle/rc/general)"
 [[ -x "$dot_rc" ]] && . "$dot_rc"
 '';
   };
