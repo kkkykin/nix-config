5ad91d468d9d13a30153aca93a4b3795acddd971
kkky <kkkykin@foxmail.com>
2025-10-18 13:39:43 +0800

    feat(sb): tun

--- modules/sing-box.nix
+++ modules/sing-box.nix
@@ -11,12 +11,19 @@
       sopsFile = ../secrets/sing-box.json;
       format = "json";
       key = "";
+      restartUnits = [ "sing-box.service" ];
     };
   };
   systemd.services.sing-box = {
     serviceConfig = {
       User = username;
-      AmbientCapabilities = ["CAP_NET_BIND_SERVICE"];
+      AmbientCapabilities = [
+        "CAP_NET_BIND_SERVICE"
+        "CAP_NET_ADMIN"
+      ];
+      CapabilityBoundingSet = [
+        "CAP_NET_ADMIN"
+      ];
       ExecStart = [
         ""
         "${pkgs.unstable.sing-box}/bin/sing-box -c ${config.sops.secrets.sing-box.path} run"
