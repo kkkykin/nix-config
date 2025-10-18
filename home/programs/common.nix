faa4d18e551a614662d06c3f051bdf8c6e0add5b
kkky <kkkykin@foxmail.com>
2025-10-18 21:14:32 +0800

    chore(): install python

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -2,8 +2,25 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  pypi-mirror = "https://pypi.mirrors.ustc.edu.cn/simple";
+in {
   home.packages = with pkgs; [
     _7zz-rar
+    python3Full
   ];
+  programs = {
+    uv = {
+      enable = true;
+      settings = {
+        python-downloads = "never";
+        python-preference = "only-system";
+        index = {
+          url = pypi-mirror;
+          default = true;
+        };
+        pip.index-url = pypi-mirror;
+      };
+    };
+  };
 }
