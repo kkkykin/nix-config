97d820bea42ad1b0c568c8e688e9f95953c5d0d9
kkky <kkkykin@foxmail.com>
2025-10-21 22:27:10 +0800

    fix(sops): dont use neededForUsers on wsl

--- users/nixos/nixos.nix
+++ users/nixos/nixos.nix
@@ -2,14 +2,8 @@
   config,
   ...
 }: {
-  sops = {
-    gnupg.home = "/root/.gnupg";
-    defaultSopsFile = ../../secrets/users.yaml;
-    secrets.nixos_pass.neededForUsers = true;
-  };
   users = {
     users.nixos = {
-      hashedPasswordFile = config.sops.secrets.nixos_pass.path;
     };
   };
 }
