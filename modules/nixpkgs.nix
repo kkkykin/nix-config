5ccf1898254e8864540ebed81cb71ef8157324f5
kkky <kkkykin@foxmail.com>
2025-11-01 01:39:13 +0800

    chore(pkgs): unstable komga

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -5,10 +5,14 @@
   nixpkgs = {
     config.allowUnfree = true;
     overlays = [
-      (final: prev: {
+      (final: prev: let
         unstable = import nixpkgs-unstable {
           inherit (final) system config;
         };
+      in {
+        unstable = unstable;
+        
+        komga = unstable.komga;
       })
     ];
   };
