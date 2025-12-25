8db82fd24d50128e2e196eaf7ec9d447b849f6f2
kkky <kkkykin@foxmail.com>
2025-12-26 00:27:51 +0800

    feat(openlist): public webdav

--- modules/nixos/openlist.nix
+++ modules/nixos/openlist.nix
@@ -1,9 +1,28 @@
 {
   config,
+  secrets,
   ...
 }: {
   services.openlist = {
     enable = true;
     envFile = config.sops.secrets.openlist.path;
+  };
+  services.cloudflared.tunnels."${secrets.cloudflared.asus.uuid}" = {
+    ingress = {
+      "openlist.${secrets.cloudflared.asus.domain}" = {
+        service = "http://127.0.0.1";
+        originRequest = {
+          httpHostHeader = "openlist.asus.local";
+        };
+      };
+    };
+  };
+
+  services.caddy.virtualHosts = {
+    "http://openlist.asus.local" = {
+      extraConfig = ''
+reverse_proxy /dav/onedrive/* http://127.0.0.1:5244
+'';
+    };
   };
 }
