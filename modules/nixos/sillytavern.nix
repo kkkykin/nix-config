4142f240ddf6fde87a712977cd927f56cb17a979
kkky <kkkykin@foxmail.com>
2026-01-09 21:37:12 +0800

    chore(sillytavern): update patch

--- modules/nixos/sillytavern.nix
+++ modules/nixos/sillytavern.nix
@@ -24,6 +24,16 @@ in {
   services.caddy.virtualHosts = {
     "http://sillytavern.asus.local" = {
       extraConfig = ''
+@openai {
+  method POST
+  path /api/openai/generate-image
+  path /api/openai/generate-video
+}
+
+json_parse @openai {
+  set reverse_proxy `"http://127.0.0.1:4000/v1"`
+}
+
 reverse_proxy ${url} {
   flush_interval -1
 }
